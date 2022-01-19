36e00d270c639f8424818b12d1c37f2b2d28f129
indrani <indranisarker7@gmail.com>
2022-01-19 22:52:40 +0600

    store add

--- FarmersHope/settings.py
+++ FarmersHope/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'BlogSupport.apps.BlogSupportConfig',
     'Users.apps.UsersConfig',
+    'store.apps.StoreConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
