45cfb61bb2a9415503149db96311e44751431892
Anam-jafar <52188853+Anam-jafar@users.noreply.github.com>
2022-01-20 13:39:22 +0600

    Blog add-update-delete implemented

--- BlogSupport/urls.py
+++ BlogSupport/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.blogDashboard, name='blog-dashboard'),
+    path('', PostListView.as_view(), name='blog-dashboard'),
     path('add_blog/', views.addBlog, name='blog-add_blog'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # pk is primary key of post object this is the convention
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
 ]
--- BlogSupport/views.py
+++ BlogSupport/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-
-blogs = [
-    {
-        'author': 'Anam',
-        'title': 'Blog Post 1',
-        'content': 'First Post content',
-        'date_posted': 'August 23, 2021'
-    },
-    {
-        'author': 'Arman',
-        'title': 'Blog Post 2',
-        'content': 'Second Post content',
-        'date_posted': 'August 25, 2021'
-    },
-
-]
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 # for viewing all the blogs
@@ -31,3 +17,56 @@ def blogDashboard(request):
 
 def addBlog(request):
     return render(request, 'BlogSupport/add_blog.html', {'title': 'Add a Question'})
+
+
+# class based view
+
+class PostListView(ListView):
+    model = Post
+    # for list and Detail
+    # <app>/<model>_<viewtype>.html -- looking for this templates if it's not the name then use this things
+    template_name = 'BlogSupport/blog_dashboard.html'
+    context_object_name = 'blogs'
+    # context object name in list View is object if we want something else then declare it here
+    ordering = ['-datePosted']  # this is for ordering - for newest to oldest
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['postTitle', 'postContent']
+
+    # for create and update
+    # <app>/<model>_form.html
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['postTitle', 'postContent']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/blog_dashboard/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
